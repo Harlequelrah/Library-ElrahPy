20ee1e24486fae6a9b7b36d1bbf9f04b7597ecc8
Harlequelrah <degbovimax19@gmail.com>
2024-10-26 14:11:58 +0000

    uploader la version 0.1.3 et modification de l'encodage du README.md

--- harlequelrah_py/setup.py
+++ harlequelrah_py/setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="harlequelrah_py",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     description="Package personnalisé pour faciliter la programmation et le développement avec python",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author="Harlequelrah",
     author_email="maximeatsoudegbovi@example.com",
-    url="https://github.com/Harlequelrah/My_Python_Library",
+    url="https://github.com/Harlequelrah/Library-harlequelrah_py",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
